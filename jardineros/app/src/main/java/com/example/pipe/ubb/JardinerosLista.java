bb43dbac699ca5bec780a5c7d18e1e4e6f319192
RudyardFuster <rudyard.fuster1401@alumnos.ubiobio.cl>
2017-06-22 10:46:29 -0400

    avance13

---
+++ jardineros/app/src/main/java/com/example/pipe/ubb/JardinerosLista.java
@@ -0,0 +1,114 @@
+package com.example.pipe.ubb;
+
+
+import android.app.Activity;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.graphics.Color;
+    import android.os.Bundle;
+    import android.util.Log;
+    import android.view.View;
+    import android.widget.AdapterView;
+    import android.widget.ArrayAdapter;
+    import android.widget.ListView;
+    import android.widget.Toast;
+
+    import java.util.ArrayList;
+    import java.util.HashMap;
+    import java.util.List;
+
+import static com.example.pipe.ubb.R.id.listview;
+
+public class JardinerosLista extends Activity {
+    ArrayList correo=new ArrayList();
+    ArrayList<String> Muestra = new ArrayList<String>();
+    String resultado;
+    String parametro="";
+    String message;
+    ListView listview;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_jardineros_lista);
+            listview = (ListView) findViewById(R.id.listview);
+
+            ArrayList lista = (ArrayList) getIntent().getSerializableExtra("micorreo");
+            final String eleccion= (String)getIntent().getSerializableExtra("op");
+
+            Log.d("mensaje","funciono: "+lista.size());
+            Log.d("mensaje","eleccion: "+eleccion);
+
+            final ArrayList<String> list = new ArrayList<String>();
+            for (int i = 0; i < lista.size(); ++i) {
+                list.add(lista.get(i).toString());
+            }
+
+            final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                    android.R.layout.simple_list_item_1, list);
+            listview.setAdapter(adapter);
+
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    //Toast.makeText(getApplicationContext(), "Click ListItem Number " + position, Toast.LENGTH_LONG).show();
+                   // view.setBackgroundColor(Color.LTGRAY);  //pone el color cuando selecciono
+
+                    Intent intent;
+
+                    if(eleccion.equals("reclamar")==true){
+                        intent = new Intent(JardinerosLista.this, Especifico.class);
+                    }
+                    else if (eleccion.equals("solicitar")==true){
+                        intent = new Intent(JardinerosLista.this, Solicitar.class);
+                    }
+                    else{ //para en caso de agun erroe tambien enviarlo a "evaluar", y que no caiga el sistema
+                        intent = new Intent(JardinerosLista.this, Main2Activity.class);
+                    }
+
+
+                    Log.d("mensaje","hola: "+listview.getAdapter().getItem(position)); // muestra el seleccionado
+
+                    //Creamos la información a pasar entre actividades
+                    Bundle b = new Bundle();
+                    b.putString("NOMBRE", listview.getAdapter().getItem(position).toString());
+
+                    //Añadimos la información al intent
+                    intent.putExtras(b);
+
+                    //Iniciamos la nueva actividad
+                    startActivity(intent);
+
+                }
+            });
+
+        }
+
+        private class StableArrayAdapter extends ArrayAdapter<String> {
+
+            HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+            public StableArrayAdapter(Context context, int textViewResourceId,
+                                      List<String> objects) {
+                super(context, textViewResourceId, objects);
+                for (int i = 0; i < objects.size(); ++i) {
+                    mIdMap.put(objects.get(i), i);
+                }
+            }
+
+            @Override
+            public long getItemId(int position) {
+                String item = getItem(position);
+                return mIdMap.get(item);
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+
+        }
+
+
+    }
--- jardineros/app/src/main/java/com/example/pipe/ubb/panel_usuario.java
+++ jardineros/app/src/main/java/com/example/pipe/ubb/panel_usuario.java
@@ -1,20 +1,41 @@
 package com.example.pipe.ubb;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Pipe on 18-06-2017.
  */
 
 public class panel_usuario extends AppCompatActivity implements View.OnClickListener  {
+    ArrayList correo=new ArrayList();
     String id_string,clave_string;
+    String resultado;
+    String parametro;
     TextView informacion;
     Button modificar;
+    private Button Puntuar;
+    private Button Reclamar;
+    private Button Solicitar;
+    String opcion="";
+
+    String  LOGIN_URL3="http://34.193.208.83/jardinero/ver.php";
+    JSONParser jsonParser = new JSONParser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +45,15 @@ public class panel_usuario extends AppCompatActivity implements View.OnClickList
         informacion = (TextView) findViewById(R.id.informacion);
         modificar=(Button)findViewById(R.id.mdatos);
         informacion.setText("Bienvenido: "+id_string);
-        modificar.setOnClickListener(this);
 
+        Puntuar=(Button)findViewById(R.id.evaluarJardinero);
+        Reclamar=(Button)findViewById(R.id.reclamos);
+        Solicitar=(Button)findViewById(R.id.solicitarJardinero);
+
+        modificar.setOnClickListener(this);
+        Puntuar.setOnClickListener(this);
+        Reclamar.setOnClickListener(this);
+        Solicitar.setOnClickListener(this);
 
     }
     public void onClick(View v) {
@@ -35,12 +63,69 @@ public class panel_usuario extends AppCompatActivity implements View.OnClickList
                 a.putExtra("id", id_string);
                 a.putExtra("clave",clave_string);
                 startActivity(a);
-                                break;
+                break;
+
+            case R.id.evaluarJardinero:
+                opcion="evaluar";
+                new panel_usuario.mostrar().execute();
+                break;
+
+            case R.id.reclamos:
+                opcion="reclamar";
+                new panel_usuario.mostrar().execute();
+                break;
+
+            case R.id.solicitarJardinero:
+                opcion="solicitar";
+                new panel_usuario.mostrar().execute();
+                break;
         }
     }
 
 
+    class mostrar extends AsyncTask<String, String, String> {
+        protected void onPreExecute() {
 
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            List parametros = new ArrayList();
+            parametros.add(new BasicNameValuePair("correo",parametro));
+            resultado = jsonParser.makeHttpRequest(LOGIN_URL3, "POST",
+                    parametros).toString();
+            JSONObject object = null;
+            try {
+                object = new JSONObject(resultado);
+                JSONArray json_array = object.optJSONArray("respuesta");
+                correo.clear();
+                for (int i = 0; i < json_array.length(); i++) {
+                    correo.add(json_array.getJSONObject(i).getString("correo"));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        protected void onPostExecute(String file_url) {
+            String valores="";
+            for(int i=0;i<correo.size();i++){
+                valores=valores+correo.get(i).toString()+" ";
+
+            }
+            Intent c = new Intent(panel_usuario.this,JardinerosLista.class);
+            c.putExtra("micorreo", correo);
+            c.putExtra("op",opcion);
+            startActivity(c);
+
+            Log.d("mensaje","hola: "+valores);
+
+
+        }
+    }
 
 
 
