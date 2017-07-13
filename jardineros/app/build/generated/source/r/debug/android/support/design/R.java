79de81c7254deb4fb58ee632238ec1fd32adaaf2
RudyardFuster <rudyard.fuster1401@alumnos.ubiobio.cl>
2017-07-13 17:08:46 -0400

    agregado list mejorado

--- jardineros/app/build/generated/source/r/debug/android/support/design/R.java
+++ jardineros/app/build/generated/source/r/debug/android/support/design/R.java
@@ -645,11 +645,11 @@ public final class R {
         public static final int design_fab_background = 0x7f020056;
         public static final int design_ic_visibility = 0x7f020057;
         public static final int design_snackbar_background = 0x7f020058;
-        public static final int navigation_empty_icon = 0x7f02005e;
-        public static final int notification_template_icon_bg = 0x7f020060;
+        public static final int navigation_empty_icon = 0x7f02006b;
+        public static final int notification_template_icon_bg = 0x7f02006f;
     }
     public static final class id {
-        public static final int action0 = 0x7f0c00b2;
+        public static final int action0 = 0x7f0c00b8;
         public static final int action_bar = 0x7f0c0065;
         public static final int action_bar_activity_content = 0x7f0c0000;
         public static final int action_bar_container = 0x7f0c0064;
@@ -658,7 +658,7 @@ public final class R {
         public static final int action_bar_subtitle = 0x7f0c0045;
         public static final int action_bar_title = 0x7f0c0044;
         public static final int action_context_bar = 0x7f0c0066;
-        public static final int action_divider = 0x7f0c00b6;
+        public static final int action_divider = 0x7f0c00bc;
         public static final int action_menu_divider = 0x7f0c0002;
         public static final int action_menu_presenter = 0x7f0c0003;
         public static final int action_mode_bar = 0x7f0c0062;
@@ -673,12 +673,12 @@ public final class R {
         public static final int beginning = 0x7f0c003b;
         public static final int bottom = 0x7f0c002a;
         public static final int buttonPanel = 0x7f0c004e;
-        public static final int cancel_action = 0x7f0c00b3;
+        public static final int cancel_action = 0x7f0c00b9;
         public static final int center = 0x7f0c002b;
         public static final int center_horizontal = 0x7f0c002c;
         public static final int center_vertical = 0x7f0c002d;
         public static final int checkbox = 0x7f0c005c;
-        public static final int chronometer = 0x7f0c00b9;
+        public static final int chronometer = 0x7f0c00bf;
         public static final int clip_horizontal = 0x7f0c0036;
         public static final int clip_vertical = 0x7f0c0037;
         public static final int collapseActionView = 0x7f0c003e;
@@ -695,7 +695,7 @@ public final class R {
         public static final int disableHome = 0x7f0c0017;
         public static final int edit_query = 0x7f0c0067;
         public static final int end = 0x7f0c002e;
-        public static final int end_padder = 0x7f0c00be;
+        public static final int end_padder = 0x7f0c00c4;
         public static final int enterAlways = 0x7f0c001d;
         public static final int enterAlwaysCollapsed = 0x7f0c001e;
         public static final int exitUntilCollapsed = 0x7f0c001f;
@@ -710,14 +710,14 @@ public final class R {
         public static final int icon = 0x7f0c004c;
         public static final int ifRoom = 0x7f0c003f;
         public static final int image = 0x7f0c0049;
-        public static final int info = 0x7f0c00bd;
+        public static final int info = 0x7f0c00c3;
         public static final int item_touch_helper_previous_elevation = 0x7f0c0005;
         public static final int left = 0x7f0c0030;
-        public static final int line1 = 0x7f0c00b7;
-        public static final int line3 = 0x7f0c00bb;
+        public static final int line1 = 0x7f0c00bd;
+        public static final int line3 = 0x7f0c00c1;
         public static final int listMode = 0x7f0c0014;
         public static final int list_item = 0x7f0c004b;
-        public static final int media_actions = 0x7f0c00b5;
+        public static final int media_actions = 0x7f0c00bb;
         public static final int middle = 0x7f0c003c;
         public static final int mini = 0x7f0c003a;
         public static final int multiply = 0x7f0c0023;
@@ -762,15 +762,15 @@ public final class R {
         public static final int src_in = 0x7f0c0026;
         public static final int src_over = 0x7f0c0027;
         public static final int start = 0x7f0c0032;
-        public static final int status_bar_latest_event_content = 0x7f0c00b4;
+        public static final int status_bar_latest_event_content = 0x7f0c00ba;
         public static final int submenuarrow = 0x7f0c005f;
         public static final int submit_area = 0x7f0c0070;
         public static final int tabMode = 0x7f0c0016;
-        public static final int text = 0x7f0c00bc;
-        public static final int text2 = 0x7f0c00ba;
+        public static final int text = 0x7f0c00c2;
+        public static final int text2 = 0x7f0c00c0;
         public static final int textSpacerNoButtons = 0x7f0c0057;
         public static final int text_input_password_toggle = 0x7f0c0098;
-        public static final int time = 0x7f0c00b8;
+        public static final int time = 0x7f0c00be;
         public static final int title = 0x7f0c004d;
         public static final int title_template = 0x7f0c0052;
         public static final int top = 0x7f0c0033;
@@ -818,7 +818,6 @@ public final class R {
         public static final int abc_search_dropdown_item_icons_2line = 0x7f040017;
         public static final int abc_search_view = 0x7f040018;
         public static final int abc_select_dialog_material = 0x7f040019;
-<<<<<<< HEAD
         public static final int design_bottom_sheet_dialog = 0x7f040024;
         public static final int design_layout_snackbar = 0x7f040025;
         public static final int design_layout_snackbar_include = 0x7f040026;
@@ -832,45 +831,18 @@ public final class R {
         public static final int design_navigation_menu = 0x7f04002e;
         public static final int design_navigation_menu_item = 0x7f04002f;
         public static final int design_text_input_password_icon = 0x7f040030;
-        public static final int notification_media_action = 0x7f040038;
-        public static final int notification_media_cancel_action = 0x7f040039;
-        public static final int notification_template_big_media = 0x7f04003a;
-        public static final int notification_template_big_media_narrow = 0x7f04003b;
-        public static final int notification_template_lines = 0x7f04003c;
-        public static final int notification_template_media = 0x7f04003d;
-        public static final int notification_template_part_chronometer = 0x7f04003e;
-        public static final int notification_template_part_time = 0x7f04003f;
-        public static final int select_dialog_item_material = 0x7f040046;
-        public static final int select_dialog_multichoice_material = 0x7f040047;
-        public static final int select_dialog_singlechoice_material = 0x7f040048;
-        public static final int support_simple_spinner_dropdown_item = 0x7f040049;
-=======
-        public static final int design_bottom_sheet_dialog = 0x7f040021;
-        public static final int design_layout_snackbar = 0x7f040022;
-        public static final int design_layout_snackbar_include = 0x7f040023;
-        public static final int design_layout_tab_icon = 0x7f040024;
-        public static final int design_layout_tab_text = 0x7f040025;
-        public static final int design_menu_item_action_area = 0x7f040026;
-        public static final int design_navigation_item = 0x7f040027;
-        public static final int design_navigation_item_header = 0x7f040028;
-        public static final int design_navigation_item_separator = 0x7f040029;
-        public static final int design_navigation_item_subheader = 0x7f04002a;
-        public static final int design_navigation_menu = 0x7f04002b;
-        public static final int design_navigation_menu_item = 0x7f04002c;
-        public static final int design_text_input_password_icon = 0x7f04002d;
-        public static final int notification_media_action = 0x7f040035;
-        public static final int notification_media_cancel_action = 0x7f040036;
-        public static final int notification_template_big_media = 0x7f040037;
-        public static final int notification_template_big_media_narrow = 0x7f040038;
-        public static final int notification_template_lines = 0x7f040039;
-        public static final int notification_template_media = 0x7f04003a;
-        public static final int notification_template_part_chronometer = 0x7f04003b;
-        public static final int notification_template_part_time = 0x7f04003c;
-        public static final int select_dialog_item_material = 0x7f040043;
-        public static final int select_dialog_multichoice_material = 0x7f040044;
-        public static final int select_dialog_singlechoice_material = 0x7f040045;
-        public static final int support_simple_spinner_dropdown_item = 0x7f040046;
->>>>>>> 18fe6de56d696ded11bd069635e564d59bc42de0
+        public static final int notification_media_action = 0x7f040039;
+        public static final int notification_media_cancel_action = 0x7f04003a;
+        public static final int notification_template_big_media = 0x7f04003b;
+        public static final int notification_template_big_media_narrow = 0x7f04003c;
+        public static final int notification_template_lines = 0x7f04003d;
+        public static final int notification_template_media = 0x7f04003e;
+        public static final int notification_template_part_chronometer = 0x7f04003f;
+        public static final int notification_template_part_time = 0x7f040040;
+        public static final int select_dialog_item_material = 0x7f040047;
+        public static final int select_dialog_multichoice_material = 0x7f040048;
+        public static final int select_dialog_singlechoice_material = 0x7f040049;
+        public static final int support_simple_spinner_dropdown_item = 0x7f04004a;
     }
     public static final class string {
         public static final int abc_action_bar_home_description = 0x7f060000;
--- jardineros/app/src/main/java/com/example/pipe/ubb/JardinerosLista.java
+++ jardineros/app/src/main/java/com/example/pipe/ubb/JardinerosLista.java
@@ -7,17 +7,22 @@ import android.app.Activity;
     import android.graphics.Color;
     import android.os.Bundle;
     import android.util.Log;
-    import android.view.View;
-    import android.widget.AdapterView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
     import android.widget.ArrayAdapter;
-    import android.widget.ListView;
-    import android.widget.Toast;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-    import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
     import java.util.HashMap;
     import java.util.List;
 
-import static com.example.pipe.ubb.R.id.listview;
 
 public class JardinerosLista extends Activity {
     ArrayList correo=new ArrayList();
@@ -26,35 +31,59 @@ public class JardinerosLista extends Activity {
     String parametro="";
     String message;
     ListView listview;
+    ListViewAdapter adapter;
+    ArrayList<String>  titulos= new ArrayList<>();
+    ArrayList<String>  subtitulos= new ArrayList<>();
+    ArrayList<String> notas = new ArrayList<>();
+
+    int[] img = {R.drawable.h1, R.drawable.m1, R.drawable.h2, R.drawable.m2, R.drawable.h3, R.drawable.m3, R.drawable.h4, R.drawable.m4, R.drawable.m5};
+    ArrayList<Integer> imagenes = new ArrayList<Integer>();
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_jardineros_lista);
             listview = (ListView) findViewById(R.id.listview);
+/*
+            imagenes.add(R.drawable.h1);
+            imagenes.add(R.drawable.h3);
+            imagenes.add(R.drawable.m3);
+            imagenes.add(R.drawable.m2);
+*/
+
 
             ArrayList lista = (ArrayList) getIntent().getSerializableExtra("micorreo");
+            ArrayList listaN = (ArrayList) getIntent().getSerializableExtra("misnombres");
+            ArrayList listaC = (ArrayList) getIntent().getSerializableExtra("misnotas");
+
             final String eleccion= (String)getIntent().getSerializableExtra("op");
             final String usuario= (String)getIntent().getSerializableExtra("usuario");
 
             Log.d("mensaje","funciono: "+lista.size());
             Log.d("mensaje","eleccion: "+eleccion);
-
+            //ArrayList<Category> category = new ArrayList<Category>();
+            //AdapterCategory adapter = new AdapterCategory(this, category);
             final ArrayList<String> list = new ArrayList<String>();
+            int k=0;
             for (int i = 0; i < lista.size(); ++i) {
                 list.add(lista.get(i).toString());
+                subtitulos.add(listaN.get(i).toString());
+                titulos.add(lista.get(i).toString());
+                notas.add("Evaluacion : "+ listaC.get(i).toString());
+                imagenes.add(img[(int) (Math.random() * 8)]);
             }
 
-            final StableArrayAdapter adapter = new StableArrayAdapter(this,
-                    android.R.layout.simple_list_item_1, list);
+            adapter = new ListViewAdapter(this, titulos, subtitulos, notas, imagenes);
+
+            //final StableArrayAdapter adapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1, list);
             listview.setAdapter(adapter);
 
             listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                     //Toast.makeText(getApplicationContext(), "Click ListItem Number " + position, Toast.LENGTH_LONG).show();
-                   // view.setBackgroundColor(Color.LTGRAY);  //pone el color cuando selecciono
+                    // view.setBackgroundColor(Color.LTGRAY);  //pone el color cuando selecciono
                     String jardinero=listview.getAdapter().getItem(position).toString();
 
                     Intent intent;
@@ -85,36 +114,76 @@ public class JardinerosLista extends Activity {
 
                     //Iniciamos la nueva actividad
                     startActivity(intent);
-
+                   // Toast.makeText(getApplicationContext(), "presiono " + position, Toast.LENGTH_SHORT).show();
                 }
             });
 
         }
 
-        private class StableArrayAdapter extends ArrayAdapter<String> {
+        //LISTVIEW  ADAPTER
+        public class ListViewAdapter extends BaseAdapter {
+            // Declare Variables
+            Context context;
+            ArrayList<String> titulos;
+            ArrayList<String> subtitulos;
+            ArrayList<String> notas;
+            ArrayList<Integer> imagenes;
+            LayoutInflater inflater;
 
-            HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+            public ListViewAdapter(Context context, ArrayList<String> titulos, ArrayList<String> subtitulos, ArrayList<String> notas, ArrayList<Integer> imagenes) {
+                this.context = context;
+                this.titulos = titulos;
+                this.subtitulos=subtitulos;
+                this.notas = notas;
+                this.imagenes = imagenes;
+            }
 
-            public StableArrayAdapter(Context context, int textViewResourceId,
-                                      List<String> objects) {
-                super(context, textViewResourceId, objects);
-                for (int i = 0; i < objects.size(); ++i) {
-                    mIdMap.put(objects.get(i), i);
-                }
+            @Override
+            public int getCount() {
+                return titulos.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return titulos.get(position);
             }
 
             @Override
             public long getItemId(int position) {
-                String item = getItem(position);
-                return mIdMap.get(item);
+                return 0;
             }
 
-            @Override
-            public boolean hasStableIds() {
-                return true;
-            }
+            public View getView(int position, View convertView, ViewGroup parent) {
 
+                // Declare Variables
+                TextView txtTitle;
+                TextView txtSubtitle;
+                TextView txtNote;
+                ImageView imgImg;
+
+                //http://developer.android.com/intl/es/reference/android/view/LayoutInflater.html
+                inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                View itemView = inflater.inflate(R.layout.jardinero_listrow, parent, false);
+
+                // Locate the TextViews in listview_item.xml
+                txtTitle = (TextView) itemView.findViewById(R.id.titulo_jardinero);
+                txtSubtitle = (TextView) itemView.findViewById(R.id.subtitulo_jardinero);
+                txtNote = (TextView) itemView.findViewById(R.id.nota_jardinero);
+
+                imgImg = (ImageView) itemView.findViewById(R.id.imagen_jardinero);
+
+                // Capture position and set to the TextViews
+                txtTitle.setText(titulos.get(position));
+                txtSubtitle.setText(subtitulos.get(position));
+                txtNote.setText(notas.get(position));
+                imgImg.setImageResource(imagenes.get(position));
+
+                return itemView;
+            }
         }
+
+
 
 
     }
